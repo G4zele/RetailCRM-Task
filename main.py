005376e41a0d566cac047efb001bb4b6563f5f70
G4zele <grandidenhic@gmail.com>
2020-10-14 22:13:59 +0300

    commit

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 client = retailcrm.v5('https://popova.retailcrm.ru', 'h1iKk1Sb0AaM66Ms86mXh7pgMaDz6wrN')
-class Answer:
-    by = 'id'
-    ids = []
 
 @app.route('/')
 def hello_world():
@@ -13,7 +10,6 @@ def hello_world():
 
 @app.route('/testd')
 def service():
-    a = Answer
     ID = request.args.get('ids%5B0%5D=')
     exID = request.args.get('externalIds%5B0%5D=')
     mode = request.args.get('mode=')
@@ -30,20 +26,20 @@ def service():
         Product_name = tmp
         Product_name = 'Чехол ' + Product_name
         result = client.products({'name':Product_name})
-        a.ids = []
+        ids = []
         n = 0
         while n < result._Response__response_body['pagination']['totalCount']:
-            a.ids.append(result._Response__response_body['products'][n]['id'])
+            ids.append(result._Response__response_body['products'][n]['id'])
             n += 1
-        return a
+        return {'by':'id', 'ids' : ids}
     elif (mode == 'serv'):
         result = client.products({'name':'гарантии'})
         a.ids = []
         n = 0
         while n < result._Response__response_body['pagination']['totalCount']:
-            a.ids.append(result._Response__response_body['products'][n]['id'])
+            ids.append(result._Response__response_body['products'][n]['id'])
             n += 1
-        return a
+        return {'by':'id', 'ids' : ids}
 
    
     
