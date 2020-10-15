2830d30222401ea4648257839c6f71c133e8ddea
G4zele <grandidenhic@gmail.com>
2020-10-15 08:24:28 +0300

    commit

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ def service():
     
     if (mode == 'access'):
         result = client.products({'externalId':exID})
-        Product_name = result._Response__response_body['products'][0]['name']
-        Product_name = (Product_name[Product_name.find(" ") + 1 : ])
-        point = Product_name.find(",") + 1
-        n = 0
-        tmp = ""
-        while n < point:
-            tmp += Product_name[n]
-            n += 1
-        Product_name = tmp
-        Product_name = 'Чехол ' + Product_name
-        result = client.products({'name':Product_name})
-        ids = []
-        n = 0
-        while n < len(result._Response__response_body['products']):
-            ids.append(result._Response__response_body['products'][n]['id'])
-            n += 1
-        return {'id':ids}
+        return result._Response__response_body
+        #Product_name = result._Response__response_body['products'][0]['name']
+        #Product_name = (Product_name[Product_name.find(" ") + 1 : ])
+        #point = Product_name.find(",") + 1
+        #n = 0
+        #tmp = ""
+        #while n < point:
+        #    tmp += Product_name[n]
+        #    n += 1
+        #Product_name = tmp
+        #Product_name = 'Чехол ' + Product_name
+        #result = client.products({'name':Product_name})
+        #ids = []
+        #n = 0
+        #while n < len(result._Response__response_body['products']):
+        #    ids.append(result._Response__response_body['products'][n]['id'])
+        #    n += 1
+        #return {'id':ids}
     elif (mode == 'serv'):
         result = client.products({'name':'гарантии'})
         ids = []
@@ -40,7 +41,7 @@ def service():
         while n < len(result._Response__response_body['products']):
             ids.append(result._Response__response_body['products'][n]['id'])
             n += 1
-        return {'by':'id','ids':ids}
+        return {'by':'externalId','ids':ids}
 
    
     
